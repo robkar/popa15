04ae3755a4acd53f0e436f5bc266a3c6d2b32777
Robert Karlsson <robkar@gmail.com>
2015-08-19 20:03:33 +0200

    fixed opengraph image

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 <meta property="og:title" content="<?php is_front_page() ? bloginfo('name') : wp_title('|', true, 'right'); ?>" />
 <?php if (is_front_page()) { ?><meta property="og:description" content="Bob Hund, Seinabo Sey, James Blake [UK], Jungle [UK], Angel Haze [US], Lorentz, Shout Out Louds, Mø [DK], Laakso, Amason, Elliphant, Tove Styrke, Mapei, Sabina Ddumba, Joel Alme och Maja Francis spelar på Popaganda 2015. Fler artister tillkommer. #popa15" />
 <?php } ?>
-<meta property="og:image" content="<?= get_template_directory_uri() ?>/img/popa15_biljettbubbla_svart.png" />
+<meta property="og:image" content="<?= get_template_directory_uri() ?>/img/anka15.min.png" />
 
 <?php wp_head(); ?>
 </head>
